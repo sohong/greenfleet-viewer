a223c727484c66cf437d72c5b402b6043dd15cde
sohong <sohongg@gmail.com>
2012-03-12 15:54:23 +0900

    xxx

--- Viewer.Common/View/VideoView.xaml.cs
+++ Viewer.Common/View/VideoView.xaml.cs
@@ -22,6 +22,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Viewer.Common.Util;
+using System.Windows.Threading;
 
 namespace Viewer.Common.View {
 
@@ -31,15 +32,37 @@ namespace Viewer.Common.View {
 
         #region fields
 
+        private DispatcherTimer m_timer;
+        private bool m_sliding;
         private double m_videoLength;
 
         #endregion // fields
+
+
+        #region events 
+
+        public event Action<VideoView, double, double> PositionChanged;
+
+        #endregion // events
 
 
         #region constructor
 
         public VideoView() {
             InitializeComponent();
+
+            m_timer = new DispatcherTimer();
+            m_timer.Interval = TimeSpan.FromMilliseconds(50);
+            m_timer.Tick += new EventHandler(timer_Tick);
+        }
+
+        void timer_Tick(object sender, EventArgs e) {
+            if (!m_sliding) {
+                timelineSlider.Value = Math.Max(timelineSlider.Value, mediaMain.Position.TotalMilliseconds);
+            }
+            if (PositionChanged != null) {
+                PositionChanged(this, m_videoLength, mediaMain.Position.TotalMilliseconds);
+            }
         }
 
         #endregion // constructor
@@ -61,15 +84,18 @@ namespace Viewer.Common.View {
             // TODO 항상 false를 리턴하고 있다.
             //if (mediaMain.HasVideo) { 
                 mediaMain.Play();
+                m_timer.Start();
             //}
         }
 
         public void Stop() {
             mediaMain.Stop();
+            m_timer.Start();
         }
 
         public void Pause() {
             mediaMain.Pause();
+            m_timer.Start();
         }
 
         public void Home() {
@@ -85,15 +111,58 @@ namespace Viewer.Common.View {
 
         #region event handlers
 
+        private void mediaMain_MediaOpened(object sender, RoutedEventArgs e) {
+            LogUtil.Debug("Media opened");
+            timelineSlider.Maximum = m_videoLength = mediaMain.NaturalDuration.TimeSpan.TotalMilliseconds;
+        }
+
+        private void mediaMain_MediaEnded(object sender, RoutedEventArgs e) {
+            Stop();
+        }
+
         private void btnPlay_Click(object sender, RoutedEventArgs e) {
             Play();
         }
 
-        #endregion // event handlers
-
-        private void mediaMain_MediaOpened(object sender, RoutedEventArgs e) {
-            LogUtil.Debug("Media opened");  
-            m_videoLength = mediaMain.NaturalDuration.TimeSpan.TotalMilliseconds;
+        private void btnStop_Click(object sender, RoutedEventArgs e) {
+            Stop();
         }
+
+        private void btnPause_Click(object sender, RoutedEventArgs e) {
+            Pause();
+        }
+
+        private void btnHome_Click(object sender, RoutedEventArgs e) {
+            Home();
+        }
+
+        private void btnPrevious_Click(object sender, RoutedEventArgs e) {
+            
+        }
+
+        private void btnNext_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void btnEnd_Click(object sender, RoutedEventArgs e) {
+            End();
+        }
+
+        private void timelineSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) {
+            //LogUtil.Debug("timeLineSlider.ValueChanged: " + timelineSlider.Value);
+            //mediaMain.Position = TimeSpan.FromMilliseconds(timelineSlider.Value);
+        }
+
+        private void timelineSlider_PreviewMouseDown(object sender, MouseButtonEventArgs e) {
+            m_sliding = true;
+        }
+
+        private void timelineSlider_PreviewMouseUp(object sender, MouseButtonEventArgs e) {
+            mediaMain.Position = TimeSpan.FromMilliseconds(timelineSlider.Value);
+            mediaMain.Play();
+            m_sliding = false;
+        }
+
+        #endregion // event handlers
     }
 }
