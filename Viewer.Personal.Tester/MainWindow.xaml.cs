095045bcf7918f4f92677e84b41c267c21855846
sohong <sohongg@gmail.com>
2012-03-12 11:37:40 +0900

    yyy

---
+++ Viewer.Personal.Tester/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿////////////////////////////////////////////////////////////////////////////////
+// MainWindow.cs
+// 2012.03.12, created by sohong
+//
+// =============================================================================
+// Copyright (C) 2012 PalmVision.
+// All Rights Reserved.
+////////////////////////////////////////////////////////////////////////////////
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Viewer.Personal.Tester {
+
+    /// <summary>
+    /// </summary>
+    public partial class MainWindow : Window {
+    
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e) {
+            Width = SystemParameters.PrimaryScreenWidth * 2 / 3;
+            Height = SystemParameters.PrimaryScreenHeight * 3 / 4;
+            Left = (SystemParameters.PrimaryScreenWidth - Width) / 2;
+            Top = (SystemParameters.PrimaryScreenHeight - Height) / 2;
+        }
+    }
+}
