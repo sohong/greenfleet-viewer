c622739a60f08f08f4be2d846667445b5d0beb15
sohong <sohongg@gmail.com>
2012-03-23 15:37:16 +0900

    xxx

--- HelloMap/MainWindow.xaml.cs
+++ HelloMap/MainWindow.xaml.cs
@@ -10,15 +10,30 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.IO;
 
 namespace HelloMap {
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
+
         public MainWindow() {
             InitializeComponent();
+
+            MapScriptHelper helper = new MapScriptHelper();
+            browser.ObjectForScripting = helper;
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "map.html");
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e) {
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "map.html");
+            browser.Navigate(path);
+        }
+
+        private void btnTest_Click(object sender, RoutedEventArgs e) {
+            browser.InvokeScript("addMarker", 111, 37.390039, 127.115263);
         }
     }
 }
---
+++ HelloMap/MapScriptHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Permissions;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+namespace HelloMap {
+
+    [PermissionSet(SecurityAction.Demand, Name="FullTrust")]
+    [ComVisible(true)]
+    public class MapScriptHelper {
+
+        public MapScriptHelper() {
+        }
+
+        public void MarkerDblClicked(string track) {
+            MessageBox.Show("marker double clicked at " + track, "marker");
+        }
+    }
+}
