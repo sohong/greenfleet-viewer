0f5216360cc82991ab37a6f9d5de6aa36cc2bf29
Sohong <sohongg@gmail.com>
2012-04-22 18:45:40 +0900

    .

--- Viewer.Common/UI/SimpleSpeedometer.cs
+++ Viewer.Common/UI/SimpleSpeedometer.cs
@@ -34,17 +34,80 @@ namespace Viewer.Common.UI
 
         public SimpleSpeedometer()
         {
+            RenderOptions.SetEdgeMode(this, EdgeMode.Unspecified);
         }
 
         #endregion // constructors
 
 
         #region properties
+
+        public double StartAngle
+        {
+            get { return m_startAngle; }
+            set
+            {
+                if (value != m_startAngle) {
+                    m_startAngle = value;
+                    InvalidateArrange();
+                }
+            }
+        }
+        private double m_startAngle;
+
+        public double EndAngle
+        {
+            get { return m_endAngle; }
+            set
+            {
+                if (value != m_endAngle) {
+                    m_endAngle = value;
+                    InvalidateArrange();
+                }
+            }
+        }
+        private double m_endAngle;
+
+        public double Minimum
+        {
+            get { return m_minimum; }
+            set
+            {
+                if (value != m_minimum) {
+                    m_minimum = value;
+                    InvalidateArrange();
+                }
+            }
+        }
+        private double m_minimum;
+
+        public double Maximum
+        {
+            get { return m_maximum; }
+            set
+            {
+                if (value != m_maximum) {
+                    m_maximum = value;
+                    InvalidateArrange();
+                }
+            }
+        }
+        private double m_maximum;
+
+        public double Value
+        {
+            get { return m_value; }
+            set
+            {
+                if (value != m_value) {
+                    m_value = value;
+                    InvalidateArrange();
+                }
+            }
+        }
+        private double m_value;
+
         #endregion // properties
-
-
-        #region overriden properties
-        #endregion // overriden properties
 
 
         #region overriden methods
@@ -56,6 +119,59 @@ namespace Viewer.Common.UI
             AddElement(m_pin = new DrawingVisual());
         }
 
+        protected override void LayoutElements(double width, double height)
+        {
+            DrawFrame(width, height);
+            DrawHand(width, height);
+            DrawPin(width, height);
+        }
+
         #endregion // overriden methods
+
+
+        #region internal methods
+
+        private void DrawFrame(double width, double height)
+        {
+        }
+
+        private void DrawHand(double width, double height)
+        {
+            DrawingContext dc = m_hand.RenderOpen();
+            double x = width / 2;
+            double y = height / 2;
+            Brush fill = new SolidColorBrush(UIElement.ToColor(0xffffffff));
+
+            Point[] pts = new Point[3];
+            pts[0] = new Point(x, y);
+            pts[1] = new Point(x - 20, y - 20);
+            pts[2] = new Point(x - 20, y + 20);
+
+            PathGeometry path = new PathGeometry();
+            PathFigure figure = new PathFigure();
+            figure.Segments.Add(new PolyLineSegment(pts, false));
+            path.Figures.Add(figure);
+            
+            dc.DrawGeometry(fill, null, path);
+            
+            dc.Close();
+        }
+
+        private void DrawPin(double width, double height)
+        {
+            DrawingContext dc = m_pin.RenderOpen();
+            double x = width / 2;
+            double y = height / 2;
+            Brush fill = new SolidColorBrush(UIElement.ToColor(0xffffffff));
+
+            double v = Math.Max(width, height);
+            double w = 7 * width / v;
+            double h = 7 * height / v;
+            dc.DrawEllipse(fill, null, new Point(x, y), w, h);
+
+            dc.Close();
+        }
+
+        #endregion // internal methods
     }
 }
